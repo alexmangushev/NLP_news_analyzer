7a3adf4d1309e70c1f2391cf4434847d7169db6a
Alexandr_Shchuk <shchukinaa2002@gmail.com>
2022-12-19 14:09:54 +0300

    Shchukin-1 мелкие изменения

--- scripts/parser.py
+++ scripts/parser.py
@@ -3,6 +3,7 @@ import re           # regular expressions lib
 import requests     # site reading lib
 import textwrap
 import os
+import parser_links_1
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from datetime import datetime
@@ -41,8 +42,9 @@ class ParsingInMongo:
         self.internal_links = []
         self.links_for_links = []
         self.photo_video_links = []
-        with open(r"C:\Users\fredo\Desktop\Work_Stereotech\repositories\NLP_news_analyzer\www.volgograd.kp.ru_internal_links.txt") as file:
-            for internal_link in file:
+        
+        links = parser_links_1.links_parser(30)["internal_urls"]
+        for internal_link in links:
                 internal_link_separate = re.findall(r"https://www.volgograd.kp.ru/daily/\d{4,7}" or 
                                                     r"https://www.volgograd.kp.ru/online/news/\d{4,7}" or
                                                     r"https://www.volgograd.kp.ru/video/" or
@@ -54,6 +56,19 @@ class ParsingInMongo:
                     self.photo_video_links.append(internal_link.strip())
                 else:
                     self.internal_links.append(internal_link.strip())
+        # with open(r"C:\Users\fredo\Desktop\Work_Stereotech\repositories\NLP_news_analyzer\www.volgograd.kp.ru_internal_links.txt") as file:
+        #     for internal_link in file:
+        #         internal_link_separate = re.findall(r"https://www.volgograd.kp.ru/daily/\d{4,7}" or 
+        #                                             r"https://www.volgograd.kp.ru/online/news/\d{4,7}" or
+        #                                             r"https://www.volgograd.kp.ru/video/" or
+        #                                             r"https://www.volgograd.kp.ru/photo/", 
+        #                                             internal_link)
+        #         if internal_link_separate == []:
+        #             self.links_for_links.append(internal_link.strip())
+        #         elif internal_link_separate[0] == "https://www.volgograd.kp.ru/video/" or internal_link_separate[0] == "https://www.volgograd.kp.ru/photo/":
+        #             self.photo_video_links.append(internal_link.strip())
+        #         else:
+        #             self.internal_links.append(internal_link.strip())
         
         # for link in self.internal_links:
         #     print(link)
@@ -106,25 +121,12 @@ class ParsingInMongo:
             dict_for_mongo['text'] = news_text
             json_for_mongo.append(dict_for_mongo)
             
+        # print(json_for_mongo)
         self.input_in_mongo(json_for_mongo)
             
     def input_in_mongo(self, json):
         compling_sema = self.collection
         compling_sema.insert_many(json)
-        
-        # self.url = url
-        # # Get path and filename for saving article by splitting URL.
-        # # If the URL ends with some.html, then the previous (-2) element
-        # # of the path is taken to form the path and the filename = some.html.txt respectively.
-        # path_arr = self.url.split('/')
-        # if path_arr[-1] != '':
-        #     self.filename = path_arr[-1] + ".txt"
-        #     self.path = os.getcwd() + "/".join(path_arr[1:-1])
-        # else:
-        #     self.filename = path_arr[-2] + ".txt"
-        #     self.path = os.getcwd() + "/".join(path_arr[1:-2])
-        # if not os.path.exists(self.path):
-        #     os.makedirs(self.path)
         
     def get_text(self):
         r = requests.get(self.url).text
@@ -142,63 +144,15 @@ class ParsingInMongo:
                         p.a.replace_with(link.text + str("[" + link['href'] + "]"))
                 # устанавливаем ширину строки равной self.wrap (по умолчанию, 80 символов)
                 wrapped_text += ''.join(textwrap.fill(p.text, self.wrap)) + "\n\n"
-        self.write_in_file(wrapped_text)
+        # self.write_in_file(wrapped_text)
         
         return wrapped_text
         
-    def write_in_file(self, text):
-        # записывает text в каталог self.path:"[CUR_DIR]/host.ru/path_item1/path_item2/..."
-        file = open(str(self.path) + '/' + str(self.filename), mode="a")
-        file.write(text)
-        file.close()
-    
-    def parser(self, url, description, tags):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        bunch_of_text = soup.find_all('div', class_='listcot col-md-9 col-sm-9 col-xs-9')
-        bunch_of_a = [c.a for c in bunch_of_text]
-        # full description
-        for c in bunch_of_a:
-            description.append(c.text + "\n" + c['href'])
-        # description = [c.text + " " + c['href'] for c in bunch_of_a] # - earlier version
-
-        # turn description text into tags
-        text = [c.text for c in bunch_of_a]
-        morph = pymorphy2.MorphAnalyzer(lang='ru')
-        #if morph.parse("1000")[0].tag.string.consist('NUMB'): print("ye")
-        for i in range(len(text)):
-            phrase = re.split('\W', text[i])
-            tags.append("")
-            for word in phrase:
-                word = morph.parse(word)[0]
-                pos = word.tag.POS
-                if pos != 'PREP' and pos != 'CONJ' and pos != 'INTJ' and pos != 'PRCL' \
-                        and pos != 'NUMR' and pos != 'INFN' and pos != 'VERB':
-                    tags[i] = tags[i] + word.normal_form + " "
-
-
-    def db_input(self, description, tags):
-        # Подключаемся к PostgreSQL на сервере
-        connection = psycopg2.connect(host=host, user=user, password=password, database=db_name)
-        for i in range(len(description)):
-            with connection.cursor() as cursor:
-                connection.autocommit = True
-                cursor.execute("""INSERT INTO answers (answer, tags) VALUES (%s, %s);""", (description[i], tags[i],))
+    # def write_in_file(self, text):
+    #     # записывает text в каталог self.path:"[CUR_DIR]/host.ru/path_item1/path_item2/..."
+    #     file = open(str(self.path) + '/' + str(self.filename), mode="a")
+    #     file.write(text)
+    #     file.close()                 
 
 obj = ParsingInMongo()
-# Parsing site
-# url = 'https://vse-kursy.com/onlain/free/?ysclid=l55k1ui5n9366579259'
-# desc = []
-# tags = []
-# obj.parser(url, desc, tags)
-# obj.db_input(desc, tags)
-
-# url = 'https://vse-kursy.com/onlain/free/page/'
-# for i in range(2, 54):
-#     url_cycle = url + str(i) + "/"
-#     desc = []
-#     tags = []
-#     obj.parser(url_cycle, desc, tags)
-#     obj.db_input(desc, tags)
-#     print(url_cycle, " is done")
 obj.get_text()
